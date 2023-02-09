2bbcd557162428ad711f8c02c40ab1b16fc34650
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-02-09 21:44:51 +0700

    adding hero files

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import './Hero.css'
+
+function Hero(props) {
+    return(
+        <>
+            <div className={props.clName}>
+                <img alt="Business Image" src={props.heroImg}></img>
+                <div className='hero-text'>
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                    <a href={props.url} className={props.btnClass}>
+                        {props.btnText}
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,19 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 function Home() {
     return(
         <>
         <Navbar />
-        <h1>This is home</h1>
+        <Hero 
+        clName="hero"
+        heroImg="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=815&q=80"
+        title="Grow Better"
+        text="We Help You With Your Brand"
+        btnText="Business Plan"
+        url="/"
+        btnClass="show"
+        />
         </>
     )
 }
