6ab789170f0299fc9d5d70d550b11d638a345742
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-02-10 21:20:03 +0700

    setting dynamic background to route

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,20 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutImg from "../assets/group-diverse-people-having-business-meeting.jpg";
 
 function About() {
     return(
         <>
         <Navbar />
-        <h1>This is About</h1>
+        <Hero 
+        clName="hero"
+        heroImg={AboutImg}
+        title="Grow Better"
+        text="We Help You With Your Brand"
+        btnText="Business Plan"
+        url="/"
+        btnClass="show"
+        />
         </>
     )
 }
